8b56263e12c68ce5b4685134e99ce69fadd88d97
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 11:03:40 +0100

    Create Suits.cs

---
+++ card-game-go-fish/Suits.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace card_game_go_fish
+{
+    enum Suits
+    {
+        Spades,
+        Clubs,
+        Diamonds,
+        Hearts,
+    }
+}
