ad4910acb646cb19cf7612f7166e510fa5eb665e
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 14:55:52 +0100

    Add plural in card names

--- card-game-go-fish/Card.cs
+++ card-game-go-fish/Card.cs
@@ -21,8 +21,30 @@ namespace card_game_go_fish
         {
             get
             {
-                return Value.ToString() + " of " + Suit.ToString();
+                return names[(int)Value] + " " + Suits[(int)Suit];
             }
         }
+
+        private static string[] names0And2AndMore = new string[]
+        {
+            "", "aces", "twos", "threes", "fours", "fives", "sixes", "sevens", "eights", "nines", "tens", "jacks", "queens", "kings"
+        };
+
+        private static string[] names1 = new string[]
+        {
+            "", "ace", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "jack", "queen", "king"
+        };
+
+        public static string Plural(Values value, int count)
+        {
+            if (count == 1)
+                return names1[(int)value];
+            else
+                return names0And2AndMore[(int)value];
+        }
+
+        private static string[] suits = new string[] { "spade", "club", "diamond", "heart" };
+        private static string[] names = new string[] { "", "Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King" };
+
     }
 }
