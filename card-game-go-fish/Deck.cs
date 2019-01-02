d2bfe18e5178bd37e7b5aa3ac79bd6db04da3b81
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 15:00:30 +0100

    Add Deck constructor

--- card-game-go-fish/Deck.cs
+++ card-game-go-fish/Deck.cs
@@ -8,6 +8,15 @@ namespace card_game_go_fish
 {
     class Deck
     {
+        private List<Card> cards;
+        private Random random = new Random();
 
+        public Deck()
+        {
+            cards = new List<Card>();
+            for (int suit = 0; suit <= 3; suit++)
+                for (int value = 1; value <= 13; value++)
+                    cards.Add(new Card((Suits)suit, (Values)value));
+        }
     }
 }
