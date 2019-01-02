87c30dc2bb4061c0c3f2fed5225f82a790c2ce8a
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 14:18:48 +0100

    Add Player constructor

--- card-game-go-fish/Player.cs
+++ card-game-go-fish/Player.cs
@@ -10,5 +10,14 @@ namespace card_game_go_fish
         private Random random;
         private Deck cards;
         private TextBox textBoxOnForm;
+
+        public Player(String name, Random random, TextBox textBoxOnForm)
+        {
+            this.name = name;
+            this.random = random;
+            this.textBoxOnForm = textBoxOnForm;
+            this.cards = new Deck(new Card[] { });
+            textBoxOnForm.Text += name + " joined the game" + Environment.NewLine;
+        }
     }
 }
