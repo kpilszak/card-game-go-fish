574cd0f6614160abc92442a7f447a290e8d0dbad
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 15:12:45 +0100

    Add methods in Deck.cs

--- card-game-go-fish/Deck.cs
+++ card-game-go-fish/Deck.cs
@@ -18,5 +18,83 @@ namespace card_game_go_fish
                 for (int value = 1; value <= 13; value++)
                     cards.Add(new Card((Suits)suit, (Values)value));
         }
+
+        public int Count { get { return cards.Count; } }
+
+        public void Add(Card cardToAdd)
+        {
+            cards.Add(cardToAdd);
+        }
+
+        public Card Deal(int index)
+        {
+            Card CardToDeal = cards[index];
+            cards.RemoveAt(index);
+            return CardToDeal;
+        }
+
+        public Card Deal()
+        {
+            return Deal(0);
+        }
+        
+        public Card Peek(int index)
+        {
+            return cards[index];
+        }
+
+        public bool ContainsValue(Values value)
+        {
+            foreach (Card card in cards)
+                if (card.Value == value)
+                    return true;
+            return false;
+        }
+
+        public Deck PullOutValues(Values value)
+        {
+            Deck deckToReturn = new Deck(new Card[] { });
+            for (int i = cards.Count - 1; i >= 0; i--)
+                if (cards.[i].Value == value)
+                    deckToReturn.Add(Deal(i));
+            return deckToReturn;
+        }
+
+        public bool HasMatch(Values value)
+        {
+            int NumberOfCards = 0;
+            foreach (Card card in cards)
+                if (card.Value == value)
+                    NumberOfCards++;
+            if (NumberOfCards == 4)
+                return true;
+            else
+                return false;
+        }
+
+        public void Shuffle()
+        {
+            List<Card> NewCards = new List<Card>();
+            while (cards.Count > 0)
+            {
+                int CardToMove = random.Next(cards.Count);
+                NewCards.Add(cards[CardToMove]);
+                cards.RemoveAt(CardToMove);
+            }
+            cards = NewCards;
+        }
+
+        public IEnumerable<string> GetCardNames()
+        {
+            string[] CardNames = new string[cards.Count];
+            for (int i = 0; i < cards.Count; i++)
+                CardNames[i] = cards[i].Name;
+            return CardNames;
+        }
+
+        public void SortByValue()
+        {
+            cards.Sort(new CardComparer_byValue());
+        }
     }
 }
