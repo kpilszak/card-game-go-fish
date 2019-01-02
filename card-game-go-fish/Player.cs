2e68bb2fa44b48c0f91d63daf10aa05223f70ea0
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 14:14:15 +0100

    Add poles in Player.cs

--- card-game-go-fish/Player.cs
+++ card-game-go-fish/Player.cs
@@ -1,7 +1,14 @@
-﻿namespace card_game_go_fish
+﻿using System;
+using System.Windows.Forms;
+
+namespace card_game_go_fish
 {
     class Player
     {
-
+        private string name;
+        public string Name { get { return name; } }
+        private Random random;
+        private Deck cards;
+        private TextBox textBoxOnForm;
     }
 }
