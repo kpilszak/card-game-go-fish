3528c218655b874ccd9f8e1414e4282aa1c46237
kpilszak <klaudia.pilszak@gmail.com>
2019-01-02 11:12:14 +0100

    Add button listener which shows random card

--- card-game-go-fish/Form1.cs
+++ card-game-go-fish/Form1.cs
@@ -17,9 +17,11 @@ namespace card_game_go_fish
             InitializeComponent();
         }
 
+        Random random = new Random();
         private void button1_Click(object sender, EventArgs e)
         {
-
+            Card card = new Card((Suits)random.Next(4), (Values)random.Next(1, 14));
+            MessageBox.Show(card.Name);
         }
     }
 }
